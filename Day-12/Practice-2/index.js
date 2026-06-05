41f2caba2dc1e9bd0eb757da626d47076ea7b2cd
Azizbek Abdullayev <azizbekabdullayev3400@gmail.com>
2026-06-05 16:29:31 +0500

    Added new items..

---
+++ Day-12/Practice-2/index.js
@@ -0,0 +1,13 @@
+// Salom!..
+// Dars vaqti: 05.06.2026y 15:00 - 16:30...
+
+// 1-mashq..
+let n = 5;
+
+let arr = []
+
+for (let i = 1; i <= 9; i += 2) {
+    arr.push(i);
+}
+console.log(arr);
+// Bajarildi!..
