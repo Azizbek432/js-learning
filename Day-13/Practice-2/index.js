cbc137a57b99821abaf70e7c217d75c4989b8d11
Azizbek Abdullayev <azizbekabdullayev3400@gmail.com>
2026-06-10 16:38:18 +0500

    Added Day-13..

---
+++ Day-13/Practice-2/index.js
@@ -0,0 +1,21 @@
+// Salom! Dars vaqti : 10.06.2026y 15:00 - 16:30 gacha..
+
+let programmer = {
+    name: "Azizbek",
+    sure: "Abdullayev",
+    age: 13,
+    job: "Full Stack Developer",
+    experience: 1,
+    languages: ["HTML", "CSS", "JavaScript", "Python", "C++", "React.js", "Next.js", "Tailwind CSS"],
+    isEmployed: true,
+    hobbies: ["Coding", "Gaming", "Traveling", "Playing chess"]
+}
+
+console.log(programmer.name); // Azizbek
+console.log(programmer.sure); // Abdullayev
+console.log(programmer.age); // 13
+console.log(programmer.job); // Full Stack Developer
+console.log(programmer.experience); // 1
+console.log(programmer.languages); // ["HTML", "CSS", "JavaScript", "Python", "C++", "React.js", "Next.js", "Tailwind CSS"]
+console.log(programmer.isEmployed); // true
+console.log(programmer.hobbies); // ["Coding", "Gaming", "Traveling", "Playing chess"]      
